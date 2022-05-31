949df85f5a66dc9a9f7c77a432b1f18209bc6f0d
Developer-Rakib <mdtazulislam6011@gmail.com>
2022-05-31 15:06:52 +0600

    home page done

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,6 +4,10 @@ import { GoMarkGithub } from 'react-icons/go';
 import { GrLinkedinOption } from 'react-icons/gr';
 import { CgFacebook } from 'react-icons/cg';
 import { AiOutlineTwitter } from 'react-icons/ai';
+import { AiOutlineHome } from 'react-icons/ai';
+import { FiUser } from 'react-icons/fi';
+import { RiContactsBookLine } from 'react-icons/ri';
+import { BiBookContent } from 'react-icons/bi';
 
 const Header = () => {
     return (
@@ -22,11 +26,11 @@ const Header = () => {
 
             <nav class="navbar">
                 <ul>
-                    <li><a href="#home">home</a></li>
-                    <li><a href="#about">about</a></li>
-                    <li><a href="#education">education</a></li>
-                    <li><a href="#portfolio">portfolio</a></li>
-                    <li><a href="#contact">contact</a></li>
+                    <li><a href="#home"><AiOutlineHome style={{marginRight:'10px'}}></AiOutlineHome> Home</a></li>
+                    <li><a href="#about"><FiUser style={{marginRight:'10px'}}></FiUser> about</a></li>
+                    {/* <li><a href="#education">education</a></li> */}
+                    <li><a href="#portfolio"><BiBookContent style={{marginRight:'10px'}}></BiBookContent> portfolio</a></li>
+                    <li><a href="#contact"><RiContactsBookLine style={{marginRight:'10px'}}></RiContactsBookLine> Contact</a></li>
                 </ul>
             </nav>
 
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,11 +4,14 @@ const Home = () => {
     return (
         <section class="home" id="home">
 
-            <h3>HI THERE !</h3>
+            <h3>React Frontend Developer</h3>
             <h1>I'M <span>Tazul Islam Rakib</span></h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio deserunt aspernatur fugiat minus enim ullam repudiandae sint sed magnam tenetur! Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, at.
             </p>
-            <a href="#about"><button class="btn">about me <i class="fas fa-user"></i></button></a>
+            <div>
+                <a style={{marginRight:'5px'}} href="#"><button class="btn-fill">Hire Me</button></a>
+                <a href="#"><button class="btn">Download Resume</button></a>
+            </div>
 
         </section>
     );
