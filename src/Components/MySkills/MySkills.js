e4c94592e7b88adc8c0395b6fe4e7f4ae3cf6c3f
Developer-Rakib <mdtazulislam6011@gmail.com>
2022-06-10 20:13:31 +0600

    skil img adde

--- src/Components/MySkills/MySkills.js
+++ src/Components/MySkills/MySkills.js
@@ -1,7 +1,35 @@
-import ProgressBar from '@ramonak/react-progress-bar';
+// import ProgressBar from '@ramonak/react-progress-bar';
 import React from 'react';
+import node from '../../img/node-removebg-preview.png';
+import html from '../../img/html5.png';
+import css from '../../img/CSS3.png';
+import mongoDB from '../../img/mongo.png';
+import js from '../../img/js-removebg-preview.png';
+import typeScript from '../../img/Typescript_logo_2020.svg-removebg-preview.png';
+import express from '../../img/express.png';
+import react from '../../img/React-icon.svg-removebg-preview.png';
+import NextJS from '../../img/1_KDMx1YspSrBcFJG-NDZgDg-removebg-preview.png';
+import firebase from '../../img/firebase-removebg-preview.png';
 
 const MySkills = () => {
+    const skillImgs = [
+        { img: html, name: "HTML" },
+        { img: css, name: "CSS" },
+        { img: "https://i.ibb.co/KDGNZDH/bootstrap.png", name: "Bootstrap" },
+        { img: "https://i.ibb.co/bQzjJ5p/material.png", name: "Material UI" },
+        { img: "https://i.ibb.co/X2VprfD/tailwind-css-removebg-preview.png", name: "Tailwind" },
+        { img: "https://i.ibb.co/LNp1Qh9/sass.png", name: "SASS" },
+        { img: js, name: "JavaScipt" },
+        { img: typeScript, name: "TypeScript" },
+        { img: react, name: "React" },
+        { img: node, name: "NodeJS" },
+        { img: express, name: "ExpressJS" },
+        { img: NextJS, name: "NextJS" },
+        { img: mongoDB, name: "MongoDB" },
+        { img: firebase, name: "Firebase" },
+        { img: "https://i.ibb.co/SmgyHtR/heroku.png", name: "Heroku" },
+        { img: "https://i.ibb.co/dWK5RCW/git.png", name: "Git" },
+    ]
     return (
         <section class="mySkill-section" id="mySkills">
 
@@ -9,12 +37,24 @@ const MySkills = () => {
             <div className='mySkill-container'>
                 <div className="skillAbout">
                     <div>
-                    <h3>My Creative Skills & Experiences.</h3>
-                    <p>My name is Md. Tazul Islam, And I'm Studying Programming Hero. Web Design & Development is my passion. I Love To Learn New Skills to Become myself a Pro Developer. Now Learning JavaScript. I Get Deeper to Understand Client's Needs & Problems. My Mission is to Satisfy Every Client. Because Client Satisfaction is My First Priority. You'll Get Lifetime Support in Any Situation, I Promise that, You'll Get Fanatical Support And Absolutely Positively On-Time Delivery.</p>
-                    <button className='btn-fill'>Read More</button>
+                        <h3>My Creative Skills & Experiences.</h3>
+                        <p>My name is Md. Tazul Islam, And I'm Studying Programming Hero. Web Design & Development is my passion. I Love To Learn New Skills to Become myself a Pro Developer. Now Learning JavaScript. I Get Deeper to Understand Client's Needs & Problems. My Mission is to Satisfy Every Client. Because Client Satisfaction is My First Priority. You'll Get Lifetime Support in Any Situation, I Promise that, You'll Get Fanatical Support And Absolutely Positively On-Time Delivery.</p>
+                        <button className='btn-fill'>Read More</button>
                     </div>
                 </div>
                 <div className="skillProgress">
+                    {
+                        skillImgs.map(skillImg => {
+                            return (
+                                <div className='skill'>
+                                    <img src={skillImg.img} alt="" />
+                                    <h3>{skillImg.name}</h3>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+                {/* <div className="skillProgress">
                     <div className='skill'>
                         <h5>HTML & CSS</h5>
                         <ProgressBar
@@ -141,7 +181,7 @@ const MySkills = () => {
                             customLabel="75%"
                         />
                     </div>
-                </div>
+                </div> */}
             </div>
 
 
